47d8669ab36aa5a3a7f0d555c089d85edf788696
surajabhiwan <suraj.abhiwan@gmail.com>
2024-06-27 15:51:24 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -85,7 +85,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "Frontend/dist", "index.html"));
 });
 
 // Modified server startup
